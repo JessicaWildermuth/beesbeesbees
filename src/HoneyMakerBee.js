7baa2cc975fdd2fd3532454790bd38eb893167f3
JessicaWildermuth <jwildermuth536@gmail.com>
2020-08-24 10:43:55 -0700

    Set prototype and constructor

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,19 @@
 var HoneyMakerBee = function() {
+  Bee.call(this);
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
 };
+
+HoneyMakerBee.prototype = Object.create(Grub);
+
+HoneyMakerBee.prototype.constuctor = Bee;
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+}
+
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot--;
+}
 
